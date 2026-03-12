a8772802926713da8ee7adf30ba68527c886cc7b
MaximumReality <lorith79@gmail.com>
2026-03-12 07:39:22 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -36,6 +36,10 @@ self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
         const clone = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
         return response;
